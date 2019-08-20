150b8b6ce18c98b8742d62d1e02a161a76bd1fef
Adam DuVander <adam.duvander@zapier.com>
2019-08-20 11:20:34 -0700

    add array

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
   <body>
     <?php
     // Using two forward slashes we create comments in php
+
+    $products['Computer']=750;
+    $products['Car']=15000;
+    $products['iPhone']=1000;
+    $products['Toaster']=75;
+
       $tax_value = .0825; // This is the tax value
       $value = 750 * (1+$tax_value); // The price of the computer multiplied by tax
       $selected_product = "Computer"; // This is the product we are looking for
