46057f1ee20ab74b96d584012e5b20b8d8fb8d8b
Adam DuVander <adam.duvander@zapier.com>
2019-08-20 11:11:27 -0700

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>This is a title</title>
+  </head>
+  <body>
+    <?php
+    // Using two forward slashes we create comments in php
+
+      echo "<h1> The Computer is $750 </h1>";
+    ?>
+  </body>
+</html>
