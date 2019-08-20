5b862d55748b623b0b5b46f234cf8932e3719ea0
Adam DuVander <adam.duvander@zapier.com>
2019-08-20 11:17:36 -0700

    add variables

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
   <body>
     <?php
     // Using two forward slashes we create comments in php
+      $tax_value = .0825; // This is the tax value
+      $value = 750 * (1+$tax_value); // The price of the computer multiplied by tax
+      $selected_product = "Computer"; // This is the product we are looking for
 
-      echo "<h1> The Computer is $750 </h1>";
+      echo "<h1> The ".$selected_product." is $".$value." after tax. </h1>";
     ?>
   </body>
 </html>
